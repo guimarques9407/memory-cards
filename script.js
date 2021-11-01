e2bed394ecfb33c33b0c87765d64a4cf4583041e
Guilherme Marques <85006401+guimarques9407@users.noreply.github.com>
2021-11-01 19:07:44 -0300

    chaneg btn events

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-const cardsContainer = document.getElementById('cards-container');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
-const currentEl = document.getElementById('current');
-const showBtn = document.getElementById('show');
-const hideBtn = document.getElementById('hide');
-const questionEl = document.getElementById('question');
-const answerEl = document.getElementById('answer');
-const addCardBtn = document.getElementById('add-card');
-const clearBtn = document.getElementById('clear');
-const clearSingleCard=document.getElementById('clear-single-card')
-const addContainer = document.getElementById('add-container');
-
+const cardsContainer = document.getElementById("cards-container");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const currentEl = document.getElementById("current");
+const showBtn = document.getElementById("show");
+const hideBtn = document.getElementById("hide");
+const questionEl = document.getElementById("question");
+const answerEl = document.getElementById("answer");
+const addCardBtn = document.getElementById("add-card");
+const addContainer = document.getElementById("add-container");
+const clearBtn = document.getElementById("clear");
 // Keep track of current card
 let currentActiveCard = 0;
 
@@ -27,11 +25,11 @@ function createCards() {
 
 // Create a single card in DOM
 function createCard(data, index) {
-  const card = document.createElement('div');
-  card.classList.add('card');
+  const card = document.createElement("div");
+  card.classList.add("card");
 
   if (index === 0) {
-    card.classList.add('active');
+    card.classList.add("active");
   }
 
   card.innerHTML = `
@@ -40,19 +38,19 @@ function createCard(data, index) {
     <p>
       ${data.question}
     </p>
-    <button id="clear-single-card" title="limpar card" class="clear btn">
-    <i class="fas fa-trash"></i>
+    <button id="clearSingleCard" title="limpar card" class="clear btn" onclick="limpouCartao()">
+    <i class="fas fa-trash"></i></button>
   </div>
   <div class="inner-card-back">
     <p>
       ${data.answer}
     </p>
+    <button id="clearSingleCard" title="limpar card" class="clear btn" onclick="limpouCartao()">
+    <i class="fas fa-trash"></i></button>
   </div>
 </div>
   `;
-  console.log(card)
-  card.addEventListener('click', () => card.classList.toggle('show-answer'));
-
+  card.addEventListener("click", () => card.classList.toggle("show-answer"));
   // Add to DOM cards
   cardsEl.push(card);
 
@@ -68,14 +66,18 @@ function updateCurrentText() {
 
 // Get cards from local storage
 function getCardsData() {
-  const cards = JSON.parse(localStorage.getItem('cards'));
+  const cards = JSON.parse(localStorage.getItem("cards"));
   return cards === null ? [] : cards;
 }
 
 // Add card to local storage
 function setCardsData(cards) {
-  localStorage.setItem('cards', JSON.stringify(cards));
+  localStorage.setItem("cards", JSON.stringify(cards));
   window.location.reload();
+}
+
+function limpouCartao() {
+  console.log("Clicou no botão de lixo");
 }
 
 createCards();
@@ -83,8 +85,8 @@ createCards();
 // Event listeners
 
 // Next button
-nextBtn.addEventListener('click', () => {
-  cardsEl[currentActiveCard].className = 'card left';
+nextBtn.addEventListener("click", () => {
+  cardsEl[currentActiveCard].className = "card left";
 
   currentActiveCard = currentActiveCard + 1;
 
@@ -92,14 +94,14 @@ nextBtn.addEventListener('click', () => {
     currentActiveCard = cardsEl.length - 1;
   }
 
-  cardsEl[currentActiveCard].className = 'card active';
+  cardsEl[currentActiveCard].className = "card active";
 
   updateCurrentText();
 });
 
 // Prev button
-prevBtn.addEventListener('click', () => {
-  cardsEl[currentActiveCard].className = 'card right';
+prevBtn.addEventListener("click", () => {
+  cardsEl[currentActiveCard].className = "card right";
 
   currentActiveCard = currentActiveCard - 1;
 
@@ -107,29 +109,29 @@ prevBtn.addEventListener('click', () => {
     currentActiveCard = 0;
   }
 
-  cardsEl[currentActiveCard].className = 'card active';
+  cardsEl[currentActiveCard].className = "card active";
 
   updateCurrentText();
 });
 
 // Show add container
-showBtn.addEventListener('click', () => addContainer.classList.add('show'));
+showBtn.addEventListener("click", () => addContainer.classList.add("show"));
 // Hide add container
-hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
+hideBtn.addEventListener("click", () => addContainer.classList.remove("show"));
 
 // Add new card
-addCardBtn.addEventListener('click', () => {
+addCardBtn.addEventListener("click", () => {
   const question = questionEl.value;
   const answer = answerEl.value;
-  
+
   if (question.trim() && answer.trim()) {
     const newCard = { question, answer };
     createCard(newCard);
 
-    questionEl.value = '';
-    answerEl.value = '';
+    questionEl.value = "";
+    answerEl.value = "";
 
-    addContainer.classList.remove('show');
+    addContainer.classList.remove("show");
 
     cardsData.push(newCard);
     setCardsData(cardsData);
@@ -137,8 +139,8 @@ addCardBtn.addEventListener('click', () => {
 });
 
 // Clear cards button
-clearBtn.addEventListener('click', () => {
+clearBtn.addEventListener("click", () => {
   localStorage.clear();
-  cardsContainer.innerHTML = '';
+  cardsContainer.innerHTML = "";
   window.location.reload();
 });
